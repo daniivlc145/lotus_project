f08da865ca7230b831822590bcc902fb5e4623f0
daniivlc145 <danielibanezlopez1@gmail.com>
2024-03-15 14:29:22 +0100

    funciones de usuario v1 hechas

--- src/db_functions/users.ts
+++ src/db_functions/users.ts
@@ -1,28 +1,107 @@
 import { supabaseClient } from '../supabase_client'
 
+interface NewUserData {
+  fullName?: string,
+  dni?: string,
+  phoneNumber?: string
+}
+
 async function signUpUser (email: string, password: string, fullName: string, dni: string, phoneNumber: string) {
-    const userData = {
-      email,
-      password,
-      options: {
-        data: {
-          full_name: fullName,
-          dni,
-          phone_number: phoneNumber
-        }
+  const userData = {
+    email,
+    password,
+    options: {
+      data: {
+        full_name: fullName,
+        dni,
+        phone_number: phoneNumber
       }
-    }
-    try {
-      const { data, error} = await supabaseClient.auth.signUp(userData)
-  
-      if (error) {
-        console.error('Error al registrar usuario:', error.message)
-        return
-      }
-      // Registro exitoso
-      console.log('Usuario registrado con éxito')
-    } catch (error) {
-      //console.error('Error inesperado:', error.message)
     }
   }
+  try {
+    const { data, error} = await supabaseClient.auth.signUp(userData)
+
+    if (error) {
+      console.error('Error al registrar usuario:', error.message)
+      return
+    }
+    // Registro exitoso
+    console.log('Usuario registrado con éxito')
+  } catch (error) {
+    console.error('Error inesperado:', (error as Error).message)
+  }
+} 
+
+async function signInUser (email: string, password: string) {
+  const signInInfo = {
+    email,
+    password
+  }
+  try {
+    const {data, error} = await supabaseClient.auth.signInWithPassword(signInInfo)
+    if (error) {
+      console.log('Error al iniciar sesión:', error.message)
+    }
+    console.log('Exito iniciando sesión')
+  } catch (error) {
+    console.log('Error inesperado:', (error as Error).message)
+  }
+}
+
+async function signOutUser () {
+  try {
+    const {error} = await supabaseClient.auth.signOut()
+    if (error) {
+      console.log('Error al iniciar sesión:', error.message)
+    }
+    console.log('Exito cerrando sesión')
+  } catch (error) {
+    console.log('Error inesperado:', (error as Error).message)
+  }
+}
+
+async function updateUserData (fullName: string, dni: string, phoneNumber: string) {
+  try{
+    const {data: {user}} = await supabaseClient.auth.getUser()
+    const updateData: NewUserData = {
+      fullName,
+      dni,
+      phoneNumber
+    }
+    
+    if (!user){
+      console.log('No se encontró el usuario')
+      return
+    }
+    const { error } = await supabaseClient
+      .from('users_info')
+      .update(updateData)
+      .eq('id', user.id)
+
+    if (error) {
+      console.log('Error al actualizar usuario')
+    }
+
+  } catch (error) {
+    console.log('Error inesperado:', (error as Error).message)
+  }
+}
+
+async function deleteUserAccount () {
+  try{
+    const {data: {user}} = await supabaseClient.auth.getUser()
+    
+    if (!user){
+      console.log('No se encontró el usuario')
+      return
+    }
+    const {error} = await supabaseClient.auth.admin.deleteUser(user.id)
+    if (error) {
+      console.log('Error al eliminar usuario:', error.message)
+    }
+    console.log('Exito eliminando usuario')
+  } catch (error) {
+    console.log('Error inesperado:', (error as Error).message)
+  }
+}
   
