105f48fe7308898b8ab83ddd3236071889ee3298
Laura Junli Illan Ferrandez <LJILLFER@upv.edu.es>
2024-04-08 19:15:16 +0200

    creado componente tutorial, añadido al css, e importada la fuente de handwritting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MapComponent } from './map/map.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'map', component: MapComponent },
+  // Add more routes here if needed
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
