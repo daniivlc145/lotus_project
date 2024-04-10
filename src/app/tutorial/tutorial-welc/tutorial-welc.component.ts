1e2a95261d1eba8b03ed18b16fef3c48d314fd58
Laura Junli Illan Ferrandez <LJILLFER@upv.edu.es>
2024-04-10 22:07:13 +0200

    no me sale lo del la navegación, voy con lo de info reciclaje

--- src/app/tutorial/tutorial-welc/tutorial-welc.component.ts
+++ src/app/tutorial/tutorial-welc/tutorial-welc.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import{Router} from '@angular/router';
 @Component({
   selector: 'app-tutorial-welc',
   templateUrl: './tutorial-welc.component.html',
@@ -7,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TutorialWelcComponent  implements OnInit {
 
-  constructor() { }
-
+  
+  constructor(){}
   ngOnInit() {}
 
 }
