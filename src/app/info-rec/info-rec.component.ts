30d92ce1a9425e5dbc16cdf8f00766d4876ca504
Laura Junli Illan Ferrandez <LJILLFER@upv.edu.es>
2024-04-11 04:02:57 +0200

    swiper info reciclaje acabado :)

--- src/app/info-rec/info-rec.component.ts
+++ src/app/info-rec/info-rec.component.ts
@@ -13,8 +13,15 @@ export class InfoRecComponent implements AfterViewInit {
   ngAfterViewInit() {
     // Configuración básica de Swiper
     const mySwiper = new Swiper('.swiper-container', {
+      effect: 'cards',
+      grabCursor: true,
       loop: true,
-      navigation: true,
+      pagination: {
+      el: '.swiper-pagination', // Elemento que contendrá la paginación
+      clickable: true, // Permite hacer clic en los puntos de paginación para navegar
+      bulletClass: 'swiper-pagination-bullet', // Clase para cada punto de paginación
+      bulletActiveClass: 'swiper-pagination-bullet-active', // Clase para el punto de paginación activo
+      },
     });
   }
 }
