df88872b5eb9b2fa94638f41e15a37e40b71e565
Max Kaidanov Solomatin <MKAISOL@upv.edu.es>
2024-04-21 19:01:01 +0200

    Metodo de filtrado en containers.ts y guardado en local de los contenedores

--- src/db_functions/containers.ts
+++ src/db_functions/containers.ts
@@ -1,4 +1,5 @@
 import { supabaseClient } from "../supabase_client";
+import {writeFileSync, readFileSync} from 'fs';
 
 interface ContainerInfo {
     container_id: number
@@ -47,7 +48,23 @@ export async function searchContainers(): Promise<{[clave: string]: ContainerInf
         console.error('Error inesperado:', (error as Error).message);
         throw error;
     }
+    
+    // Una vez se guarda el diccionario con todos los contenedores, lo transformamos a un objeto local
+    const path : string = "../utils/diccionario_contenedores.json"
+    
+    const data : string = JSON.stringify(result,null,2)
+
+    try{
+        writeFileSync(path,data, {flag: "w+"})
+        console.log("Diccionario creado correctamente")
+    }
+    catch(error){
+        console.error("Error inesperado: ", (error as Error).message)
+        throw error
+    }
+    
     return result;
+
 }
 export async function checkLevel(containerId: number, containerType: string): Promise<boolean> {
     if (containerType === 'Residuos Urbanos' || containerType === 'Organico' || containerType === 'Papel / Carton' || containerType === 'Envases Ligeros') {
@@ -85,4 +102,18 @@ export async function modifyLevel(container_id: number, container_type: string, 
         throw error;
     }
     
+}
+
+export async function filtrarMapa(contenedores : {[clave: string]: boolean}) : Promise<{[clave: string]: ContainerInfo[]}>{
+    const data : string =  readFileSync("../utils/diccionario_contenedores",{encoding : "utf-8"})
+    const diccionario : {[clave: string]: ContainerInfo[]} = JSON.parse(data)
+    const res : {[clave:string]: ContainerInfo[]}= {}
+    for(const element in contenedores){
+        let value = contenedores[element]
+        
+        if(value){
+            res[element] = diccionario[element]
+        }
+    }
+    return res;
 }
