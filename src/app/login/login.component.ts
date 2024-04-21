9ac199ca57ae27dd58492cc07e31921b8ac7de8a
JaviLatorre <javierlatorrecano@gmail.com>
2024-04-21 16:16:32 +0200

    parte funcionalidad login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'; // Importa el módulo Router
 import { signInUser } from './login.functions';
 
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -10,30 +9,19 @@ import { signInUser } from './login.functions';
 })
 export class loginComponent  implements OnInit {
 
-  constructor( private router: Router) { } // Inyecta el servicio Router en el constructor
-
-
-
+  constructor(private router: Router) { } // Inyecta el servicio Router en el constructor
+  errorMessage: string | null = null; // Esta es la propiedad que mencionaste
   ngOnInit() {}
-
-
-
-   signIn() {
-    
+  async signIn() {
     const email = (document.getElementById('email') as HTMLInputElement).value;
     const password = (document.getElementById('password') as HTMLInputElement).value;
-     
-    signInUser(email, password);
-      
-      
-    this.router.navigate(['/map']);
-
-
-    
+    try{
+      await signInUser(email,password);
+      console.log('goToLoginPage() called');
+      this.router.navigate(['/map']); 
+    }catch(error){
+      console.error('ERROR CAPTURADO:', (error as Error).message)
+      this.errorMessage = (error as Error).message; // Actualiza el mensaje de error
+    }
 }
-    
-  
-  
-
-
 }
