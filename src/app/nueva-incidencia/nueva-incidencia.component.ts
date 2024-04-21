7ae679527ae67db5ab7da993a4f8d15c63511d72
JaviLatorre <javierlatorrecano@gmail.com>
2024-04-21 15:10:08 +0200

    hecho header y footer del mapa

--- src/app/nueva-incidencia/nueva-incidencia.component.ts
+++ src/app/nueva-incidencia/nueva-incidencia.component.ts
@@ -56,11 +56,11 @@ export class NuevaIncidenciaComponent  implements OnInit {
     }
  
     toggleDropdownVisibility(show: boolean) {
-      const dropdownElement = document.querySelector('.dropdownCalles');
+     const dropdownElement = document.querySelector('.dropdownCalles');
       if (show) {
-        dropdownElement.classList.add('show');
+       // dropdownElement.classList.add('show');
       } else {
-        dropdownElement.classList.remove('show');
+      //  dropdownElement.classList.remove('show');
       }
     }
  
