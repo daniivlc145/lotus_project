5cc61038cd8a76cdb37afbf5b10acc6cf27c243c
Max Kaidanov Solomatin <MKAISOL@upv.edu.es>
2024-04-21 16:43:16 +0200

    Pruebas

--- src/db_functions/pruebas.ts
+++ src/db_functions/pruebas.ts
@@ -1,9 +1,10 @@
 import { signUpUser } from "src/app/registro/registro.functions";
 import { updateUserData } from "./users";
 import { signInUser } from "../app/login/login.functions";
+import { forgotPassword } from "../app/login/login.functions";
 
+//signUpUser("maxkaidanov2@gmail.com","000000", "M")
 
-signUpUser("maxkaidanov2@gmail.com","000000", "M")
-
-//signInUser("danielibanezlopez1@gmail.com","123456").then(() => updateUserData("Carlitos gordo fumon"))
+// signInUser("danielibanezlopez1@gmail.com","123456").then(() => updateUserData("Carlitos gordo GORDO"))
+forgotPassword("info2maxkaidanov2017@gmail.com")
 
