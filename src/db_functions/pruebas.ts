38ab1201c57a79a3300ae761ae26156d7da24ff1
daniivlc145 <danielibanezlopez1@gmail.com>
2024-04-21 17:01:06 +0200

    funciones inquiries

--- src/db_functions/pruebas.ts
+++ src/db_functions/pruebas.ts
@@ -3,7 +3,7 @@ import { updateUserData } from "./users";
 import { signInUser } from "../app/login/login.functions";
 
 
-signUpUser("maxkaidanov2@gmail.com","000000", "M")
+
 
 //signInUser("danielibanezlopez1@gmail.com","123456").then(() => updateUserData("Carlitos gordo fumon"))
 
