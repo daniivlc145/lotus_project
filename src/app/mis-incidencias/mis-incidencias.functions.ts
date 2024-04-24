f27600789f2c95788a8c79975432dc1bb60aa20c
daniivlc145 <danielibanezlopez1@gmail.com>
2024-04-24 15:22:03 +0200

    .

--- src/app/mis-incidencias/mis-incidencias.functions.ts
+++ src/app/mis-incidencias/mis-incidencias.functions.ts
@@ -3,15 +3,16 @@ import { supabaseClient } from "src/supabase_client";
 export async function muestraMisIncidencias(){
     try{
         let result = []
-        const {data: {user}} = await supabaseClient.auth.getUser()
-        if (!user) {
-            throw new Error('No se ha encontrado un usuario autenticado')
-        }
-
+        // const {data: {user}} = await supabaseClient.auth.getUser()
+        // if (!user) {
+        //     throw new Error('No se ha encontrado un usuario autenticado')
+        // }
+        // CODIGO A QUITAR
+        let user_id = '81ea3dda-ebf9-4a86-8a4f-7e19aaed7312'
         const {data, error} = await supabaseClient
             .from('inquiries')
             .select('created_at, description, type, geo_shape')
-            .eq('creator_id', user.id)
+            .eq('creator_id', user_id)
             .in('type', ['reclamation', 'suggestion', 'query']);
 
         if (error) {
