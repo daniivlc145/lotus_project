bd6de5a9d079726039f3e4a0ecdbc43756e325e4
Max Kaidanov Solomatin <MKAISOL@upv.edu.es>
2024-04-24 14:40:06 +0200

    Arreglado registro en el registro.component.httml y registro.component.ts

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -14,14 +14,26 @@ export class RegistroComponent  implements OnInit {
   errorMessage: string | null = null; // Esta es la propiedad que mencionaste
   oculto :boolean = false;
   ngOnInit() {}
-  async signUp() {
+
+  llamada(): void{
+    this.signUp().then(() => this.openDialog)
+  }
+
+  async signUp(): Promise<void> {
     const email = (document.getElementById('correo') as HTMLInputElement).value;
     const password = (document.getElementById('password') as HTMLInputElement).value;
     const fullName = (document.getElementById('nombre') as HTMLInputElement).value;
     const phoneNumber = (document.getElementById('telefono') as HTMLInputElement).value;
     const rep = (document.getElementById('rep') as HTMLInputElement).value;
     try{
-      console.log("LLAMADA HTML");
+      if(this.camposVacios(email,password)){throw new Error('Rellena todos los campos')}
+      if(this.validarNombre(fullName)){throw new Error('Escribe un nombre válido')}
+      if(this.validarTelefono(phoneNumber)){throw new Error('Teléfono incorrecto. Escribe solo dígitos')}
+      if(this.validarCorreoElectronico(email)){throw new Error('Email incorrecto. Escribe un email válido')}
+      if(this.validarContrasena(password,rep)){throw new Error('Las contraseñas no coinciden')}
+      if(this.validarLongitudContrasena(password)){throw new Error('La contrasña debe tener 8-16 caracteres')}
+      
+      console.log("Intento Registrar")
       await signUpUser(email,password,fullName,phoneNumber,rep);
       console.log('goToLoginPage() called');
       this.openDialog();
@@ -39,5 +51,32 @@ export class RegistroComponent  implements OnInit {
         route: '/login'
       }
     })
+  }
+  validarCorreoElectronico(correo: string): boolean {
+    console.log("validando email")
+  const expresionRegular = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return !expresionRegular.test(correo);
+  }
+  camposVacios(email: string, password: string): boolean {
+  console.log("validando email")
+  return email.trim() === '' || password.trim() === '';
+  }
+  validarNombre(texto: string): boolean {
+    const contieneDigitos = /\d/.test(texto);
+    const masDe100Caracteres = texto.length > 100;
+    return contieneDigitos || masDe100Caracteres;
+  }
+  
+  validarTelefono(texto: string): boolean {
+    const soloDigitos = /^\d+$/.test(texto);
+    return !soloDigitos;
+  }
+  validarContrasena(password: string, rep: string): boolean {
+    const contrasenaInvalida = password === rep;
+    return !contrasenaInvalida;
+  }
+  validarLongitudContrasena(password: string): boolean {
+    const longitudValida = password.length >= 8 && password.length <= 16;
+    return !longitudValida;
   }
 }
--- src/app/registro/registro.functions.ts
+++ src/app/registro/registro.functions.ts
@@ -14,13 +14,6 @@ export async function signUpUser (email: string, password: string, fullName: str
     }
   }
   try {
-    if(await camposVacios(email,password)){throw new Error('Rellena todos los campos')}
-    if(await validarNombre(fullName)){throw new Error('Escribe un nombre válido')}
-    if(await validarTelefono(phoneNumber)){throw new Error('Teléfono incorrecto. Escribe solo dígitos')}
-    if(await validarCorreoElectronico(email)){throw new Error('Email incorrecto. Escribe un email válido')}
-    if(await validarContrasena(password,rep)){throw new Error('Las contraseñas no coinciden')}
-    if(await validarLongitudContrasena(password)){throw new Error('La contrasña debe tener 8-16 caracteres')}
-
     const { data, error} = await supabaseClient.auth.signUp(userData)
     if (error) {
       throw new Error('Email o teléfono registrado/s')
@@ -30,32 +23,4 @@ export async function signUpUser (email: string, password: string, fullName: str
         console.error('Error inesperado:', (error as Error).message)
         throw error
     }
-}
-
-function validarCorreoElectronico(correo: string): boolean {
-  console.log("validando email")
-const expresionRegular = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-return !expresionRegular.test(correo);
-}
-function camposVacios(email: string, password: string): boolean {
-console.log("validando email")
-return email.trim() === '' || password.trim() === '';
-}
-function validarNombre(texto: string): boolean {
-  const contieneDigitos = /\d/.test(texto);
-  const masDe100Caracteres = texto.length > 100;
-  return contieneDigitos || masDe100Caracteres;
-}
-
-function validarTelefono(texto: string): boolean {
-  const soloDigitos = /^\d+$/.test(texto);
-  return !soloDigitos;
-}
-function validarContrasena(password: string, rep: string): boolean {
-  const contrasenaInvalida = password === rep;
-  return !contrasenaInvalida;
-}
-function validarLongitudContrasena(password: string): boolean {
-  const longitudValida = password.length >= 8 && password.length <= 16;
-  return !longitudValida;
 }
