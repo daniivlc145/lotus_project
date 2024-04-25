2aa285d7a5c0175d4c63357ed79476982c576433
Max Kaidanov Solomatin <MKAISOL@upv.edu.es>
2024-04-25 18:02:43 +0200

    Metodo de comprobacion de exitencia de correo entre otras cosas

---
+++ src/app/introducir-email/introducir-email.functions.ts
@@ -0,0 +1,35 @@
+import { supabaseClient } from "../../supabase_client"
+
+export  async function existsEmail(email : string) : Promise<boolean> {
+    try{
+        const {data, error} = await supabaseClient.rpc('exists_email',{email_to_check: email})
+        if(error){
+            throw error
+        }
+        console.log("Comprobado existencia de email")
+        return Boolean(data)
+    }
+    catch(error){
+        console.error("Error inesperado: ", (error as Error).message)
+        throw error
+    }
+}
+
+
+export async function forgotPassword(email:string) : Promise<void>{
+    try{
+        const { data, error } = await supabaseClient.auth.resetPasswordForEmail(email, {
+            // Habrá que modificar invent por la ruta correcta
+            redirectTo: 'http://localhost:4200/contrasenaOlv',
+        })
+  
+        if(error){
+            throw new Error('Error al encontrar el correo')
+        }
+        console.log("Correo encontrado, mensaje enviado")
+    }
+    catch(error){
+        console.error("Error inesperado:", (error as Error).message)
+        throw error
+    }
+  }
---
+++ src/db_functions/probarCorreo.ts
@@ -0,0 +1,3 @@
+import { signUpUser } from "../app/registro/registro.functions";
+
+signUpUser("carlitoscebellan@gmail.com","000000", "Carlitos prueba Correo", "23476781")
