d7125694d9c0909df444627219446dd45f8cf81a
JaviLatorre <javierlatorrecano@gmail.com>
2024-04-28 22:49:56 +0200

    altre

--- src/app/sugerencia/sugerencia.component.ts
+++ src/app/sugerencia/sugerencia.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PopinfoOneComponent } from '../popinfo-one/popinfo-one.component';
+import { PopoverController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sugerencia',
@@ -7,8 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SugerenciaComponent  implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private popovercntrl: PopoverController) { }
 
   ngOnInit() {}
 
+
+  async showPop(){
+    const popover = await this.popovercntrl.create({
+      component: PopinfoOneComponent,
+      backdropDismiss:false,
+      componentProps: {
+        title: '¡Sugerencia enviada!',
+        content: 'Hemos recibido tu sugerencia. Nos pondremos con ella lo antes posible. ¡Gracias!'
+      }
+    });
+    await popover.present();
+
+    return popover.onWillDismiss().then(() => {
+      console.log('Navegando a: /ruta-deseada');
+      this.router.navigateByUrl('/profUser');
+    });
+  }
 }
