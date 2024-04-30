4f45e47907aa7c99ab5d338440fdc7da0b2a18cf
Max Kaidanov Solomatin <MKAISOL@upv.edu.es>
2024-04-30 18:48:09 +0200

    Mostrar todas las incidencias

---
+++ src/app/vista-inc-org/vista-inc-org.functions.ts
@@ -0,0 +1,63 @@
+import { supabaseClient } from "../../supabase_client";
+
+export async function getAllInquiries() : Promise<{[clave:string]:string}[]> {
+    try{
+        let result : {[clave:string]:string}[] = []
+        
+        const {data: {user}, error: errorUsuario} = await supabaseClient.auth.getUser()
+        
+        if (errorUsuario) {
+             throw new Error('No se ha encontrado un usuario autenticado')
+         }
+        // CODIGO A QUITAR
+        console.log("Usuario encontrado")
+        
+        const {data, error} = await supabaseClient
+            .from('inquiries')
+            .select('created_at, description, type, geo_shape')
+            .in('type', ['reclamation', 'suggestion', 'query']);
+
+        if (error) {
+            console.error('Error al obtener las incidencias:', error.message);
+            throw error;
+        }
+        
+        // Ordenar las incidencias por fecha y hora más reciente
+        data.sort((a, b) => {
+            const dateA = new Date(a.created_at).getTime();
+            const dateB = new Date(b.created_at).getTime();
+            return dateB - dateA;
+        });
+        
+        for (let elem of data){
+            let [date, hour] = elem.created_at.split('T');
+            let [year, month, day] = date.split('-');
+            let fecha = `${day}-${month}-${year}`;
+            let [hours, minutes] = hour.split(':');
+            let hora = `${hours}:${minutes}`;
+            if (elem.type === 'CONTENEDOR LLENO') {
+                elem.description = 'El contenedor está lleno.';
+            }
+            if(elem.type=== 'RECLAMACIÓN/INFORME'){
+                elem.type='RECLAMACIÓN';
+            }
+            elem.type = elem.type === 'reclamation' ? 'RECLAMACIÓN' 
+                : elem.type === 'suggestion' ? 'PETICIÓN' 
+                : 'CONSULTA'
+            result.push({
+                'fecha': fecha,
+                'hora': hora,
+                'descripcion': elem.description,
+                'type': elem.type,
+                'geo_shape': elem.geo_shape
+            })
+           
+        }
+        return result
+
+    } catch (error) {
+        console.error('Error inesperado:', (error as Error).message);
+        throw error;
+    }
+}
+
