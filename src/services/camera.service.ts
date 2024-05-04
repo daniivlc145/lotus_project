9305f55177ec3cf6394ae42cd77b6aef7d9ecdd1
daniivlc145 <danielibanezlopez1@gmail.com>
2024-05-04 10:31:05 +0200

    .

--- src/services/camera.service.ts
+++ src/services/camera.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Camera, CameraResultType, Photo, CameraSource } from '@capacitor/camera';
+import {} from '@capacitor/app';
+
+
 
 @Injectable({
   providedIn: 'root'
@@ -7,7 +10,7 @@ import { Camera, CameraResultType, Photo, CameraSource } from '@capacitor/camera
 export class CameraService {
 
   constructor() { }
-  async takePicureFromCamera(): Promise<Photo> {
+  async takePictureFromCamera(): Promise<Photo> {
     const image = await Camera.getPhoto({
         quality: 90,
         allowEditing: true,
@@ -17,14 +20,33 @@ export class CameraService {
       });
       return image;
   }
-  async takePicureFromAlbum(): Promise<Photo> {
-    const image = await Camera.getPhoto({
-        quality: 90,
-        allowEditing: true,
-        resultType: CameraResultType.Uri,
-        source: CameraSource.Photos,
-
-      });
-      return image;
-  }
+  // async requestCameraPermission() {
+  //   try {
+  //     const permission = await Camera.requestPermissions();
+  //     if(permission.camera === 'granted'){
+  //       return true
+  //     } else if(permission.photos === 'prompt'){
+        
+  //     } else{
+  //       return false
+  //     }
+  //   } catch (error) {
+  //     console.error(error);
+  //     throw error;
+  //   }
+  // }
+  // async checkCameraPermission() {
+  //   try {
+  //     const permission = await Camera.checkPermissions();
+  //     if (permission.camera === 'granted') {
+  //       return true;
+  //     } else if (permission.camera === 'denied') {
+  //       return false;
+  //     } else {
+  //       await this.requestCameraPermission()
+  //     }
+  //   } catch (error) {
+  //     throw new Error('No se tiene permiso para acceder a la cámara')
+  //   }
+  // }
 }
